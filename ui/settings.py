4d4182b3e6812772dd1ca9ad8a8ea8e038cae1b7
Melvin Hillsman <mhillsma@redhat.com>
2022-02-16 22:29:15 -0600

    Add django-formtools via pipenv
    Signed-off-by: Melvin Hillsman <mhillsma@redhat.com>

--- ui/settings.py
+++ ui/settings.py
@@ -62,7 +62,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.google'
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
